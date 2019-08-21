85a6399c75f2f3891ceff00a5dfe6fe3565f5c1e
squishy123 <christian.gnaw@gmail.com>
2019-08-21 17:33:51 -0400

    Added and fixed tests

--- tests/routes/users/loginUser.test.js
+++ tests/routes/users/loginUser.test.js
@@ -14,19 +14,36 @@ async function loginUser(username, email, password) {
 
 test(`all null values`, () => {
     return loginUser(null, null, null).then(data => {
-        expect(data).toContain({
+        expect(data).toMatchObject({
             message: "Error: Missing required parameter username/email and password"
         });
     });
 });
 
-test(`email password`, () => {
-    return loginUser(null, "christian.gnaw@gmail.com", "Flyingunicorn456!").then(
+test(`user null`, () => {
+    return loginUser("tester", null, null).then(data => {
+        expect(data).toMatchObject({
+            message: "Error: Missing required parameter password"
+        });
+    });
+});
+
+test(`pass null`, () => {
+    return loginUser(null, null, "t12211221T!").then(data => {
+        expect(data).toMatchObject({
+            message: "Error: Missing required parameter username or email"
+        });
+    });
+});
+
+test(`user password`, () => {
+    return loginUser("tester", null, "t12211221T!").then(
         data => {
-            expect(data).toContainEqual({
-                message: "Success: Successfully Logged in User"
-            });
-            expect(data).toHaveProperty("accessToken", "idToken");
+            expect(data).toHaveProperty(
+                "message", "Success: Successfully Logged in User"
+            );
+            expect(data.payload).toHaveProperty("accessToken");
+            expect(data.payload).toHaveProperty("idToken");
         }
     );
 });
