f3a3bfc94f20f5246de4cdc208ea5560df1a8e2e
squishy123 <christian.gnaw@gmail.com>
2019-08-21 15:51:56 -0400

    Fixed up validation

--- src/app/middlewares/auth/validateUserLogin.js
+++ src/app/middlewares/auth/validateUserLogin.js
@@ -1,12 +1,17 @@
 
-function ValidateUserLogin (req, res) {
-    if(!req.params.password) {
+function ValidateUserLogin(req, res) {
+    if (!req.params.password && !req.params.username && !req.params.email)
+        return res.status(400).send({
+            message: "Error: Missing required parameter username/email and password"
+        });
+
+    if (!req.params.password) {
         return res.status(400).send({
             message: "Error: Missing required parameter password"
         });
     }
 
-    if(!req.params.username && !req.params.email) {
+    if (!req.params.username && !req.params.email) {
         return res.status(400).send({
             message: "Error: Missing required parameter username or email"
         });
